9f548b88bc236aee232d4461f6e4ce02b11dcf06
Sebastian Hahner <hi@sebinside.de>
2023-09-12 23:42:50 +0200

    Fix config for github pages deployment.

--- StreamAwesome/vite.config.ts
+++ StreamAwesome/vite.config.ts
@@ -5,9 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/StreamAwesome/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
