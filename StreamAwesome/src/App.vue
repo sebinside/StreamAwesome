fead70a81554419baa1549b9df46a1df301d5fd9
Sebastian Hahner <hi@sebinside.de>
2023-09-17 14:18:12 +0200

    Add minimal example.

--- StreamAwesome/src/App.vue
+++ StreamAwesome/src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <div class="m-auto mt-10 max-w-lg rounded-2xl bg-gray-800 p-4 text-white">
+    <RouterView />
+  </div>
 </template>
-
-<style scoped></style>
--- StreamAwesome/src/components/TheWelcome.vue
+++ StreamAwesome/src/components/TheWelcome.vue
@@ -1,6 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+function alertHelloWorld() {
+  window.alert('Hello World!')
+}
+</script>
 
 <template>
-  <h1>Stream Awesome</h1>
+  <h1 class="text-3xl font-bold">Stream Awesome</h1>
   <RouterLink to="/about">About</RouterLink>
+
+  <canvas id="symbol" width="256" height="256" class="rounded-3xl border border-cyan-500"></canvas>
+  <button @click="alertHelloWorld">Generate icon</button>
 </template>
