c776b77ba5b163859da637b3ef5ef3e6bd01f9be
strudelbeast <dev@fasig.at>
2024-06-24 01:14:48 +0200

    refactor: Preset definition
    * wrap component in div to apply to vue/no-multiple-template-root convention
    * Presets are not depending on v-if structure anymore
    * move temporary fallback into own file

---
+++ StreamAwesome/src/components/settings/presets/OtherComponent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// Temporary Preset till more are implemented
+
+import type { CustomIcon } from '@/model/customIcon'
+
+// disable for demo purposes
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = defineProps<{
+  icon: CustomIcon
+}>()
+</script>
+
+<template>
+  <div class="text-sm">Currently nothing else available.</div>
+</template>
