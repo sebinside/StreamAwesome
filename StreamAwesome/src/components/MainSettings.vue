40bbd7cd67df2a74a24a774bb08a93e8b81d6011
MelanX <info@melanx.de>
2025-08-03 16:49:31 +0200

    Add basic support for user-defined presets storing in localStorage

--- StreamAwesome/src/components/MainSettings.vue
+++ StreamAwesome/src/components/MainSettings.vue
@@ -2,14 +2,20 @@
 import IconBrowser from '@/components/browser/IconBrowser.vue'
 import IconCanvas from '@/components/IconCanvas.vue'
 import IconSettings from '@/components/settings/IconSettings.vue'
+import UserUserPresetManager from '@/components/utils/UserPresetManager.vue'
 import { getMatchingGenerator } from '@/logic/generator/generators'
 import { useIconsStore } from '@/stores/icons'
+import type { CustomIcon } from '@/model/customIcon'
 
 const iconStore = useIconsStore()
 
 function downloadIcon() {
   const iconGenerator = getMatchingGenerator(iconStore.currentIcon)
   iconGenerator.saveIcon(iconStore.currentIcon)
+}
+
+function loadPreset(preset: CustomIcon<any>) {
+  Object.assign(iconStore.currentIcon, preset)
 }
 </script>
 
@@ -21,6 +27,12 @@ function downloadIcon() {
         class="mb-5 mt-5 place-self-center md:place-self-auto"
         @download-icon="downloadIcon"
       />
+      <div class="mb-3">
+        <UserUserPresetManager
+            :icon="iconStore.currentIcon"
+            @load-preset="loadPreset"
+        />
+      </div>
       <IconSettings :icon="iconStore.currentIcon" @download-icon="downloadIcon" />
     </div>
     <div class="flex-grow">
