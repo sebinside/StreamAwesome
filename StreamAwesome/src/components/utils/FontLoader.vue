957fa55e05f90835d013fd360be2b53d151fe0ba
Micha Huhn <michahuhndev@gmx.de>
2025-08-24 23:10:49 +0200

    refactor: move `useFontLoading` code into `FontLoader.vue`

--- StreamAwesome/src/components/utils/FontLoader.vue
+++ StreamAwesome/src/components/utils/FontLoader.vue
@@ -1,7 +1,28 @@
 <script setup lang="ts">
-import { useFontLoading } from '@/composables/useFontLoading.ts'
+import { useFontsStatusStore } from '@/stores/fontStatus.ts'
+import { onMounted } from 'vue'
 
-useFontLoading()
+// Cross-browser waiting for font loading is messed up, see: https://stackoverflow.com/questions/5680013/how-to-be-notified-once-a-web-font-has-loaded/77481922#77481922
+const waitForFontsLoaded = document.fonts?.ready.then(() => {
+  return new Promise((resolve) => {
+    let waitTimeMs = 5
+    const checkFontsLoaded = () => {
+      if (document.fonts?.status === 'loaded') {
+        return resolve(null)
+      }
+      waitTimeMs *= 2
+      return setTimeout(checkFontsLoaded, waitTimeMs)
+    }
+    setTimeout(checkFontsLoaded, 5)
+  })
+})
+
+const fontStatusStore = useFontsStatusStore()
+
+onMounted(async () => {
+  await waitForFontsLoaded
+  fontStatusStore.setFontsLoaded()
+})
 </script>
 
 <template>
