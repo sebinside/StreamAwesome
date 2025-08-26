8ce7a9d7d88f91927c0e7df578b9ba8a17444934
Sebastian Hahner <hi@sebinside.de>
2025-08-26 15:17:16 +0200

    Move persistence handling from URLHandler.

--- StreamAwesome/src/logic/URLHandler.ts
+++ StreamAwesome/src/logic/URLHandler.ts
@@ -1,9 +1,7 @@
 import { useIconsStore } from '@/stores/icons'
 import { watch } from 'vue'
 import { useUrlSearchParams, type UrlParams } from '@vueuse/core'
-import type { ColorSpace, CustomIcon, FontAwesomePreset } from '@/model/customIcon'
-import type { FontAwesomeFamily, FontAwesomeStyle } from '@/model/fontAwesomeConstants'
-import type { FontAwesomeIcon } from '@/model/fontAwesomeIcon'
+import { PersistenceHandler } from './persistence/PersitenceHandler'
 
 export class URLHandler {
   public static initialize(triggerIconDownload: () => void) {
@@ -13,10 +11,10 @@ export class URLHandler {
 
   private static readURLAndUpdateIcon(triggerIconDownload: () => void) {
     const params = useUrlSearchParams('history')
-    if (params.fontsize !== undefined) {
+    if (params.version !== undefined) {
       console.log('Found URL parameters. Trying to parse input...')
 
-      const icon = URLHandler.convertURLToCustomIcon(params)
+      const icon = PersistenceHandler.convertPersistentIconToIcon(params)
 
       if (icon !== null) {
         console.log('Successfully parsed icon from URL parameters.')
@@ -32,99 +30,22 @@ export class URLHandler {
     }
   }
 
-  private static convertURLToCustomIcon(params: UrlParams): CustomIcon<FontAwesomePreset> | null {
-    try {
-      const icon: CustomIcon<FontAwesomePreset> = {
-        fontSize: URLHandler.extractNumber(params, 'fontsize'),
-        fontAwesomeIcon: URLHandler.extractFontAwesomeIcon(params),
-        presetSettings: URLHandler.extractPreset(params)
-      }
-
-      return icon
-    } catch (error) {
-      console.error('Error during URL parameter parsing.', error)
-      return null
-    }
-  }
-
-  private static extractFontAwesomeIcon(params: UrlParams): FontAwesomeIcon {
-    return {
-      id: URLHandler.extractString(params, 'fontawesomeicon.id'),
-      label: URLHandler.extractString(params, 'fontawesomeicon.label'),
-      unicode: URLHandler.extractString(params, 'fontawesomeicon.unicode'),
-      isBrandsIcon: URLHandler.extractBoolean(params, 'fontawesomeicon.isbrandsicon'),
-      style: URLHandler.extractString(params, 'fontawesomeicon.style') as FontAwesomeStyle,
-      family: URLHandler.extractString(params, 'fontawesomeicon.family') as FontAwesomeFamily
-    }
-  }
-
-  private static extractPreset(params: UrlParams): CustomIcon<FontAwesomePreset>['presetSettings'] {
-    const preset = URLHandler.extractString(params, 'presetsettings.preset')
-    const typedPreset = (preset.charAt(0).toUpperCase() + preset.slice(1)) as FontAwesomePreset
-
-    switch (typedPreset) {
-      case 'Classic':
-        return {
-          preset: 'Classic',
-          hue: URLHandler.extractNumber(params, 'presetsettings.hue')
-        }
-      case 'Modern':
-        return {
-          preset: 'Modern',
-          inverted: URLHandler.extractBoolean(params, 'presetsettings.inverted')
-        }
-      case 'Neo':
-        return {
-          preset: 'Neo',
-          invertDirection: URLHandler.extractBoolean(params, 'presetsettings.invertdirection'),
-          symbolOnly: URLHandler.extractBoolean(params, 'presetsettings.symbolonly'),
-          hueStart: URLHandler.extractNumber(params, 'presetsettings.huestart'),
-          hueShift: URLHandler.extractNumber(params, 'presetsettings.hueshift'),
-          saturation: URLHandler.extractNumber(params, 'presetsettings.saturation'),
-          translation: URLHandler.extractNumber(params, 'presetsettings.translation'),
-          lightness: URLHandler.extractNumber(params, 'presetsettings.lightness'),
-          colorSpace: URLHandler.extractString(params, 'presetsettings.colorspace') as ColorSpace
-        }
-      case 'Custom':
-        return {
-          preset: 'Custom',
-          backgroundColor: URLHandler.extractString(params, 'presetsettings.backgroundcolor'),
-          foregroundColor: URLHandler.extractString(params, 'presetsettings.foregroundcolor')
-        }
-      default:
-        throw new Error(`Unknown preset type: ${typedPreset}`)
-    }
-  }
-
-  private static extractNumber(params: UrlParams, parameterName: string): number {
-    const parsedNumber = parseFloat(params[parameterName] as string)
-    if (isNaN(parsedNumber)) {
-      throw new Error(`${parameterName} in URL parameters is not a valid number.`)
-    }
-    return parsedNumber
-  }
-
-  private static extractString(params: UrlParams, parameterName: string): string {
-    const value = params[parameterName]
-    if (value === undefined || value === null || Array.isArray(value)) {
-      throw new Error(`${parameterName} in URL parameters is not a valid string.`)
-    }
-    return value as string
-  }
-
-  private static extractBoolean(params: UrlParams, parameterName: string): boolean {
-    const value = params[parameterName]
-    if (value === undefined || value === null || Array.isArray(value)) {
-      throw new Error(`${parameterName} in URL parameters is not a valid boolean.`)
-    }
-    return value.toLowerCase() === 'true'
-  }
-
   private static watchIconAndUpdateURL() {
     watch(useIconsStore().currentIcon, (newIcon) => {
       const params = useUrlSearchParams('history')
       URLHandler.clearURLParameters(params)
-      URLHandler.mapObjectToParams(newIcon, '', params)
+      const persistentIcon = PersistenceHandler.convertIconToPersistentIcon(newIcon)
+
+      for (const key in persistentIcon) {
+        if (persistentIcon.hasOwnProperty(key)) {
+          if (typeof persistentIcon[key] === 'string') {
+            params[key] = persistentIcon[key] as string
+          } else {
+            console.warn(`Unexpected type for key "${key}":`, persistentIcon[key])
+            params[key] = JSON.stringify(persistentIcon[key])
+          }
+        }
+      }
     })
   }
 
@@ -132,26 +53,6 @@ export class URLHandler {
     for (const key in params) {
       if (params.hasOwnProperty(key)) {
         delete params[key]
-      }
-    }
-  }
-
-  private static mapObjectToParams(
-    obj: Record<string, unknown>,
-    prefix: string | null,
-    params: UrlParams
-  ) {
-    for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        const value = obj[key]
-
-        const joinedKey = [prefix, key].filter(Boolean).join('.').toLowerCase()
-
-        if (typeof value === 'object' && value !== null) {
-          URLHandler.mapObjectToParams(value as Record<string, unknown>, joinedKey, params)
-        } else {
-          params[joinedKey] = `${value}`.toLowerCase()
-        }
       }
     }
   }
---
+++ StreamAwesome/src/logic/persistence/converter/IconConverter310.ts
@@ -0,0 +1,69 @@
+import { PersistentIconConverter } from '../PersistentIconConverter'
+
+export class IconConverter310 extends PersistentIconConverter {
+  public convertPersistentIconToIcon(
+    record: Record<string, unknown>
+  ): Record<string, unknown> | null {
+    try {
+      const icon: Record<string, unknown> = {
+        fontSize: this.extractNumber(record, 'fontsize'),
+        fontAwesomeIcon: this.extractFontAwesomeIcon(record),
+        presetSettings: this.extractPreset(record)
+      }
+
+      return icon
+    } catch (error) {
+      console.error('Error during persistent icon conversion.', error)
+      return null
+    }
+  }
+
+  private extractFontAwesomeIcon(record: Record<string, unknown>): Record<string, unknown> {
+    return {
+      id: this.extractString(record, 'fontawesomeicon.id'),
+      label: this.extractString(record, 'fontawesomeicon.label'),
+      unicode: this.extractString(record, 'fontawesomeicon.unicode'),
+      isBrandsIcon: this.extractBoolean(record, 'fontawesomeicon.isbrandsicon'),
+      style: this.extractString(record, 'fontawesomeicon.style'),
+      family: this.extractString(record, 'fontawesomeicon.family')
+    }
+  }
+
+  private extractPreset(record: Record<string, unknown>): Record<string, unknown> {
+    const preset = this.extractString(record, 'presetsettings.preset')
+    const typedPreset = preset.charAt(0).toUpperCase() + preset.slice(1)
+
+    switch (typedPreset) {
+      case 'Classic':
+        return {
+          preset: 'Classic',
+          hue: this.extractNumber(record, 'presetsettings.hue')
+        }
+      case 'Modern':
+        return {
+          preset: 'Modern',
+          inverted: this.extractBoolean(record, 'presetsettings.inverted')
+        }
+      case 'Neo':
+        return {
+          preset: 'Neo',
+          invertDirection: this.extractBoolean(record, 'presetsettings.invertdirection'),
+          symbolOnly: this.extractBoolean(record, 'presetsettings.symbolonly'),
+          hueStart: this.extractNumber(record, 'presetsettings.huestart'),
+          hueShift: this.extractNumber(record, 'presetsettings.hueshift'),
+          saturation: this.extractNumber(record, 'presetsettings.saturation'),
+          translation: this.extractNumber(record, 'presetsettings.translation'),
+          lightness: this.extractNumber(record, 'presetsettings.lightness'),
+          colorSpace: this.extractString(record, 'presetsettings.colorspace')
+        }
+      case 'Custom':
+        return {
+          preset: 'Custom',
+          backgroundColor: this.extractString(record, 'presetsettings.backgroundcolor'),
+          foregroundColor: this.extractString(record, 'presetsettings.foregroundcolor')
+        }
+      default:
+        throw new Error(`Unknown preset type: ${typedPreset}`)
+    }
+  }
+}
