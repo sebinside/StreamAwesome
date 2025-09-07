9f922432d712289000fc03f49174119f01a357d0
Sebastian Hahner <hi@sebinside.de>
2025-09-07 11:50:39 +0200

    Improve download and copy button UI.

--- StreamAwesome/src/components/settings/DownloadButton.vue
+++ StreamAwesome/src/components/settings/DownloadButton.vue
@@ -16,18 +16,18 @@ whenever(copyShortcutIsPressed, copyIconToClipboard)
   <div class="mt-10 flex">
     <button
       type="button"
-      class="mb-2 w-full cursor-pointer rounded-l-lg border-r-2 border-gray-700 bg-gradient-to-r from-cyan-500 to-blue-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:from-teal-500 focus:ring-2 focus:ring-cyan-300 focus:outline-none dark:focus:ring-cyan-800"
+      class="mb-2 w-full cursor-pointer rounded-l-lg border-r-2 border-gray-700 bg-gradient-to-r from-cyan-500 to-blue-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:from-teal-500"
       @click="$emit('downloadIcon')"
     >
       <i class="fa-solid fa-download"></i> Download
     </button>
     <button
       type="button"
-      class="mb-2 w-full cursor-pointer rounded-r-lg bg-gradient-to-r from-blue-500 to-purple-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:to-pink-500 focus:ring-2 focus:ring-cyan-300 focus:outline-none dark:focus:ring-cyan-800"
+      class="mb-2 w-full cursor-pointer rounded-r-lg bg-gradient-to-r from-blue-500 to-purple-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:to-pink-500"
       @click="copyIconToClipboard"
     >
       <span v-if="!iconIsCopiedToClipboard"><i class="fa-solid fa-copy"></i> Copy</span>
-      <span v-else>Copied</span>
+      <span v-else><i class="fa-solid fa-check"></i> Copied</span>
     </button>
   </div>
 </template>
