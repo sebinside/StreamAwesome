d4fdd3bb1c334f08a379158750b33312e53dbd03
Micha Huhn <michahuhndev@gmx.de>
2026-01-28 14:33:16 +0100

    refactor: create global `.color-range-input` class

--- StreamAwesome/src/components/settings/presets/ClassicPreset.vue
+++ StreamAwesome/src/components/settings/presets/ClassicPreset.vue
@@ -32,62 +32,8 @@ const currentHue = ref((currentIcon.value as CustomIcon<'Classic'>).presetSettin
     id="hueSelector"
     max="360"
     min="0"
-    class="selector focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:outline-none"
+    class="color-range-input focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:outline-none"
     v-model.number="(currentIcon as CustomIcon<'Classic'>).presetSettings.hue"
     @input="currentHue = (currentIcon as CustomIcon<'Classic'>).presetSettings.hue"
   />
 </template>
-
-<style scoped>
-.selector {
-  display: block;
-  height: 1rem;
-  width: 100%;
-  cursor: pointer;
-  -webkit-appearance: none;
-  appearance: none;
-  border-radius: 0.25rem;
-  outline: 2px solid transparent;
-  outline-offset: 2px;
-}
-
-#hueSelector {
-  background: linear-gradient(
-    90deg,
-    hsl(0, 72%, 56%) 0%,
-    hsl(60, 72%, 56%) 25%,
-    hsl(120, 72%, 56%) 33.3%,
-    hsl(240, 72%, 56%) 66.6%,
-    hsl(360, 72%, 56%) 100%
-  );
-}
-
-#hueSelector::-webkit-slider-thumb {
-  -webkit-appearance: none;
-  width: 20px;
-  height: 20px;
-  border-radius: 9999px;
-  background-color: hsl(v-bind('currentHue'), 72%, 56%);
-  border: 2px solid white;
-  box-shadow: 0 0 0 2px rgba(0, 0, 0, 0.15);
-}
-
-/* ---------- Firefox only ---------- */
-#hueSelector::-moz-range-track {
-  height: 8px;
-  background: transparent;
-  border: none;
-}
-#hueSelector::-moz-range-progress {
-  height: 8px;
-  background: transparent;
-}
-#hueSelector::-moz-range-thumb {
-  width: 20px;
-  height: 20px;
-  border-radius: 9999px;
-  background: hsl(v-bind('currentHue'), 72%, 56%);
-  border: 2px solid white;
-  box-shadow: 0 0 0 2px rgba(0, 0, 0, 0.15);
-}
-</style>
--- StreamAwesome/src/components/settings/presets/NeoPreset.vue
+++ StreamAwesome/src/components/settings/presets/NeoPreset.vue
@@ -45,7 +45,7 @@ const toggleSettings = () => {
     id="hueSelector"
     min="0"
     max="360"
-    class="selector focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:outline-none"
+    class="color-range-input focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:outline-none"
     v-model.number="(currentIcon as CustomIcon<'Neo'>).presetSettings.hueStart"
     @input="currentHue = (currentIcon as CustomIcon<'Neo'>).presetSettings.hueStart"
   />
@@ -124,7 +124,7 @@ const toggleSettings = () => {
       max="1"
       min="0.05"
       step="0.01"
-      class="selector focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:outline-none"
+      class="color-range-input focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:outline-none"
       v-model.number="(currentIcon as CustomIcon<'Neo'>).presetSettings.saturation"
     />
 
@@ -139,7 +139,7 @@ const toggleSettings = () => {
       max="0.95"
       min="0.05"
       step="0.01"
-      class="selector focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:outline-none"
+      class="color-range-input focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:outline-none"
       v-model.number="(currentIcon as CustomIcon<'Neo'>).presetSettings.lightness"
     />
 
@@ -161,29 +161,6 @@ const toggleSettings = () => {
 </template>
 
 <style scoped>
-.selector {
-  display: block;
-  height: 1rem;
-  width: 100%;
-  cursor: pointer;
-  -webkit-appearance: none;
-  appearance: none;
-  border-radius: 0.25rem;
-  outline: 2px solid transparent;
-  outline-offset: 2px;
-}
-
-#hueSelector {
-  background: linear-gradient(
-    90deg,
-    hsl(0, 72%, 56%) 0%,
-    hsl(60, 72%, 56%) 25%,
-    hsl(120, 72%, 56%) 33.3%,
-    hsl(240, 72%, 56%) 66.6%,
-    hsl(360, 72%, 56%) 100%
-  );
-}
-
 #saturationSelector {
   background: linear-gradient(
     90deg,
@@ -198,34 +175,5 @@ const toggleSettings = () => {
     hsl(v-bind('currentHue'), 72%, 5%) 0%,
     hsl(v-bind('currentHue'), 72%, 95%) 100%
   );
-}
-
-#hueSelector::-webkit-slider-thumb {
-  -webkit-appearance: none;
-  width: 20px;
-  height: 20px;
-  border-radius: 9999px;
-  background-color: hsl(v-bind('currentHue'), 72%, 56%);
-  border: 2px solid white;
-  box-shadow: 0 0 0 2px rgba(0, 0, 0, 0.15);
-}
-
-/* ---------- Firefox only ---------- */
-#hueSelector::-moz-range-track {
-  height: 8px;
-  background: transparent;
-  border: none;
-}
-#hueSelector::-moz-range-progress {
-  height: 8px;
-  background: transparent;
-}
-#hueSelector::-moz-range-thumb {
-  width: 20px;
-  height: 20px;
-  border-radius: 9999px;
-  background: hsl(v-bind('currentHue'), 72%, 56%);
-  border: 2px solid white;
-  box-shadow: 0 0 0 2px rgba(0, 0, 0, 0.15);
 }
 </style>
